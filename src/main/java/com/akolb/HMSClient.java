3c4bdafd1af6d86aa1e6e8d750d50e129f904e68
Peter Vary <pvary@cloudera.com>
2018-02-08 11:45:02 +0100

    Adding rename table test

--- src/main/java/com/akolb/HMSClient.java
+++ src/main/java/com/akolb/HMSClient.java
@@ -374,6 +374,16 @@ final class HMSClient implements AutoCloseable {
     }
   }
 
+  void alterTableNoException(@NotNull String dbName, @NotNull String tableName,
+                             @NotNull Table newTable) {
+    try {
+      client.alter_table(dbName, tableName, newTable);
+    } catch (TException e) {
+      throw  new RuntimeException(e);
+    }
+  }
+
+
   private TTransport open(HiveConf conf, @NotNull URI uri) throws
       TException, IOException, LoginException {
     boolean useSasl = conf.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL);
