445704533d5710fd93e055b5e082ace361b499a0
Alexander Kolbasov <akolb@cloudera.com>
2018-07-07 16:41:59 -0700

    Support for showing databases in JSON

--- metastore-cli/src/main/java/org/apache/hadoop/hive/metastore/tools/MetastoreTool.java
+++ metastore-cli/src/main/java/org/apache/hadoop/hive/metastore/tools/MetastoreTool.java
@@ -18,10 +18,17 @@
 
 package org.apache.hadoop.hive.metastore.tools;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.thrift.TException;
 import picocli.CommandLine;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import static org.apache.hadoop.hive.metastore.tools.Constants.HMS_DEFAULT_PORT;
 import static picocli.CommandLine.Command;
@@ -34,16 +41,16 @@ import static picocli.CommandLine.ParentCommand;
  */
 @SuppressWarnings({"squid:S106", "squid:S1148"}) // Using System.out
 @Command(name = "MetastoreTool",
-    mixinStandardHelpOptions = true, version = "1.0",
-    subcommands = {HelpCommand.class,
-        MetastoreTool.DbCommand.class,
-        MetastoreTool.TableCommand.class},
-    showDefaultValues = true)
+        mixinStandardHelpOptions = true, version = "1.0",
+        subcommands = {HelpCommand.class,
+                MetastoreTool.DbCommand.class,
+                MetastoreTool.TableCommand.class},
+        showDefaultValues = true)
 
 public class MetastoreTool implements Runnable {
   @Option(names = {"-H", "--host"},
-      description = "HMS Host",
-      paramLabel = "URI")
+          description = "HMS Host",
+          paramLabel = "URI")
   private String host;
   @Option(names = {"-P", "--port"}, description = "HMS Server port")
   private Integer port = HMS_DEFAULT_PORT;
@@ -64,7 +71,7 @@ public class MetastoreTool implements Runnable {
   }
 
   @Command(name = "db",
-      subcommands = {HelpCommand.class, DbCommand.DbListCommand.class})
+          subcommands = {HelpCommand.class, DbCommand.DbListCommand.class, DbCommand.DbShowCommand.class})
   static class DbCommand implements Runnable {
 
     @ParentCommand
@@ -73,14 +80,26 @@ public class MetastoreTool implements Runnable {
     @Option(names = {"-d", "--db"}, description = "Database name pattern")
     private String dbName;
 
+    String getHost() {
+      return parent.host;
+    }
+
+    int getPort() {
+      return parent.port;
+    }
+
+    String getDbName() {
+      return dbName;
+    }
+
     static void printDbList(HMSClient client, String dbName) throws TException {
       client.getAllDatabases(dbName).forEach(System.out::println);
     }
 
     @Override
     public void run() {
-      String host = parent.host;
-      int port = parent.port;
+      String host = getHost();
+      int port = getPort();
       try (HMSClient client = new HMSClient(Util.getServerUri(host, String.valueOf(port)))) {
         printDbList(client, dbName);
       } catch (URISyntaxException e) {
@@ -94,8 +113,7 @@ public class MetastoreTool implements Runnable {
      * List databases.
      */
     @Command(name = "list",
-        description = "List all databases, optionally matching pattern",
-        subcommands = {HelpCommand.class})
+            description = "List all databases, optionally matching pattern")
     static class DbListCommand implements Runnable {
 
       @ParentCommand
@@ -103,11 +121,43 @@ public class MetastoreTool implements Runnable {
 
       @Override
       public void run() {
-        String host = parent.parent.host;
-        int port = parent.parent.port;
-        String dbName = parent.dbName;
+        String host = parent.getHost();
+        int port = parent.getPort();
+        String dbName = parent.getDbName();
+
         try (HMSClient client = new HMSClient(Util.getServerUri(host, String.valueOf(port)))) {
           printDbList(client, dbName);
+        } catch (URISyntaxException e) {
+          System.out.println("invalid host " + host);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    @Command(name = "show",
+            description = "Show all databases, optionally matching pattern")
+    static class DbShowCommand implements Runnable {
+
+      @ParentCommand
+      private DbCommand parent;
+
+      @Override
+      public void run() {
+        String host = parent.getHost();
+        int port = parent.getPort();
+        String dbName = parent.getDbName();
+
+        try (HMSClient client = new HMSClient(Util.getServerUri(host, String.valueOf(port)))) {
+          Set<String> dbNames = client.getAllDatabases(dbName);
+          List<Database> databases = new ArrayList<>(dbNames.size());
+          for (String name : dbNames) {
+            databases.add(client.getDatabase(name));
+          }
+          Gson gson = new GsonBuilder().setPrettyPrinting().create();
+          System.out.println(gson.toJson(databases,
+                  new TypeToken<List<Database>>() {
+                  }.getType()));
         } catch (URISyntaxException e) {
           System.out.println("invalid host " + host);
         } catch (Exception e) {
@@ -118,8 +168,8 @@ public class MetastoreTool implements Runnable {
   }
 
   @Command(name = "table",
-      subcommands = {HelpCommand.class, TableCommand.TablePrintCommand.class},
-      description = "HMS Table operations")
+          subcommands = {HelpCommand.class, TableCommand.TablePrintCommand.class},
+          description = "HMS Table operations")
   static class TableCommand implements Runnable {
 
     @ParentCommand
@@ -131,9 +181,9 @@ public class MetastoreTool implements Runnable {
     static void printTableList(HMSClient client, String dbName, String tableName) throws TException {
       for (String database : client.getAllDatabases(dbName)) {
         client.getAllTables(database, tableName).stream()
-            .sorted()
-            .map(t -> database + "." + t)
-            .forEach(System.out::println);
+                .sorted()
+                .map(t -> database + "." + t)
+                .forEach(System.out::println);
       }
     }
 
